aa092033bcaa5cd8dd881a09f5c27ced4962a6de
Rohitjargaal <rohitjargaal@gmail.com>
2025-05-31 16:54:58 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import HomePage from './pages/Home/HomePage'
+import HomePage from './pages/User/HomePage'
 import Navbar from './components/Navbar'
 import { Routes, Route } from "react-router-dom"
 import Footer from './components/Footer'
